062b8aa4be7c59ad1b8f5b6ce038751ef4506fe6
Ericcentrik <enaiman1@gmail.com>
2019-12-03 20:13:26 -0500

    set up html structure and created question in question.js

---
+++ assets/js/questions.js
@@ -0,0 +1,48 @@
+// list of all questions and answers
+
+var questions = [
+{
+    question:"which mens tennis player has won the highest number of grand slams?",
+    choices: ["Rafael Nadal", "Pete Sampras", "Roger Federer", "Novak Djokovic"],
+    answer:"Roger Federer"
+},
+{
+    question:"How many dots are on two dice??",
+    choices: ["Fourty two (42)", "Fourty eight(48)", "thirty eight (38)", "fifty(50)"],
+    answer:"Fourty two (42)"
+},
+{
+    question:"Which country is known as the Pearl of Africa?",
+    choices: ["Ethopia", "Morocco", "Uganda", "Tanazania"],
+    answer:"Uganda"
+},
+{
+    question:"A couple celebrating their crystal wedding anniversary have been married for how many years?",
+    choices: ["Ten (10)", "Fifteen(15)", "Twenty(20)", "Fifty(50)"],
+    answer:"Fifteen(15)"
+},
+{
+    question:"In which 1979 film was the spaceship called Nostromo?",
+    choices: ["Alien", "Star Wars", "Black Hole", "Preditor"],
+    answer:"Alien"
+},
+{
+    question:"Who was once known as the King of Soul, and now known as the Godfather of Soul?",
+    choices: ["BB King", "Micheal Jackson", "Artist formally known as Prince", "James Brown"],
+    answer: "James Brown"
+},
+{
+    question: "How many men have walked on the moon?",
+    choices: ["Four(4)", "Two(2)", "Twelve(12)", "Eight(8)"],
+    answer:"Twelve(12)"
+},
+{
+    question: "In which country would one find 8 of the world’s 10 highest mountains?",
+    choices: ["Four(4)", "Two(2)", "Twelve(12)", "Eight(8)"],
+    answer:"Napal",
+},
+
+
+
+
+]
